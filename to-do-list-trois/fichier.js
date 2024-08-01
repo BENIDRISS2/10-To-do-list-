9a71dbb114958c3d1d17a8501a78f059b473bbfc
El Bachir <benidrissr2000@gmail.com>
2024-08-01 10:37:53 +0200

    troisieme-commit-3-to-do

---
+++ to-do-list-trois/fichier.js
@@ -0,0 +1,40 @@
+class Todos extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={
+            inputTodo:"",
+            Todos:[]
+        }
+    }
+    render(){
+    return(
+       <div className="container mt-4">
+        <h1>to do list</h1>
+        <p>{this.state.inputTodo}</p>
+        <div className="input-group mb-3">
+            <input
+            type="texte"
+            value={this.state.inputTodo}
+            onChange={(e)=>{
+                this.setState({inputTodo:e.target.value})
+            }}
+            className="form-control"
+            placeholder="Ajouter une tache"
+             />
+             <button
+              className="btn btn-primary" 
+              onClick={()=>{
+            this.setState({Todos:[...this.state.Todos, this.state.inputTodo]})
+                    }}>Ajouter </button>
+        </div>
+        <ul className="list-group">
+                    {this.state.Todos.map((Todo)=>{
+                            return <li>{Todo}</li>
+                        })      }
+                </ul>
+       </div> 
+    )
+}
+}
+ReactDOM.render(<Todos/>,document.getElementById('rool'))
+
