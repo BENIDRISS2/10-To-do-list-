2bd2de40f3a1dee09963e6f7401a335980f246d6
El Bachir <benidrissr2000@gmail.com>
2024-08-01 13:40:56 +0200

    septieme-ajout

---
+++ to-do-list-07/07-fichier.js
@@ -0,0 +1,41 @@
+class Sept extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={
+            inputTodo:"",
+            Todos:[]
+        }
+    }
+    render(){
+    return(
+        <div className="container mt-4">
+            <h1>My 7eme to do list</h1>
+            <p>{this.state.inputTodo}</p>
+            <div className="input-group mb-3">
+                <input
+                type="texte"
+                value={this.state.inputTodo}
+                onChange={(e)=>{
+                  this.setState({inputTodo: e.target.value})  
+                }}
+                className="form-control"
+                placeholder="veuilllez ajouter "
+                />
+                <button
+                className="btn btn-primary"
+                onClick={()=>{
+                    this.setState({Todos:[...this.state.Todos ,this.state.inputTodo]})
+                }}>Ajouter</button>
+            </div>
+            <ul className="list-group">
+                {
+                    this.state.Todos.map((todo)=>{
+                        return <li>{todo}</li>
+                    })
+                }
+            </ul>
+
+        </div>
+    )}
+}
+ReactDOM.render(<Sept/>,document.getElementById('yist'))
